bc876cf477049129cac80e3e0c83843bf9e03f39
Uliana Kalashnikova <k1431002@gmail.com>
2020-05-29 16:24:31 +0700

    Implement test function perimeter

--- test/geometry_tests.c
+++ test/geometry_tests.c
@@ -45,3 +45,14 @@ CTEST(parse_coord, incorrect_y)
     size_t result = parse_coord(&i, &circle[0].x, line);
     ASSERT_EQUAL(expect, result);
 }
+
+CTEST(perimeter, test)
+{
+    create_circle(circle, 1, 1, 2, 1.5);
+    double expect = 3 * M_PI;
+    double result = perimeter(circle, 1);
+    ASSERT_DBL_NEAR_TOL(expect, result, 1);
+}
+
+
+
