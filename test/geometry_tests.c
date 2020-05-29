d500316f82e45548702dabe6fe7478aff4c4ae9d
Uliana Kalashnikova <k1431002@gmail.com>
2020-05-29 15:43:21 +0700

    Implement tests function parse_coord

--- test/geometry_tests.c
+++ test/geometry_tests.c
@@ -1,1 +1,47 @@
+#include "circle_proc.h"
 #include <ctest.h>
+
+figure circle[1000];
+
+void create_circle(figure* circle, size_t n, int p1, int p2, double p3)
+{
+    circle[n].x = p1;
+    circle[n].y = p2;
+    circle[n].r = p3;
+}
+
+CTEST(parse_coord, correct_x)
+{
+    char* line = "circle (1 2, 3)\n";
+    size_t expect = 0;
+    size_t i = 8;
+    size_t result = parse_coord(&i, &circle[0].x, line);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(parse_coord, incorrect_x)
+{
+    char* line = "circle (a 2, 3)\n";
+    size_t expect = 1;
+    size_t i = 8;
+    size_t result = parse_coord(&i, &circle[0].x, line);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(parse_coord, correct_y)
+{
+    char* line = "circle (1 2, 3)\n";
+    size_t expect = 0;
+    size_t i = 10;
+    size_t result = parse_coord(&i, &circle[0].x, line);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(parse_coord, incorrect_y)
+{
+    char* line = "circle (1 b, 3)\n";
+    size_t expect = 1;
+    size_t i = 10;
+    size_t result = parse_coord(&i, &circle[0].x, line);
+    ASSERT_EQUAL(expect, result);
+}
